cbe04396797cd6c52d88e58c06749040b285514a
Tim Sanders <tsanders1114@gmail.com>
2021-04-01 19:56:24 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey hey hey I'am here to learn");
+        }
+    }
+}
